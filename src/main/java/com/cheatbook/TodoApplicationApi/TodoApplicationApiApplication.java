e24eb5db416b427ec13a99f03190faba669e46e5
cheatbook <km1994321@yahoo.co.jp>
2021-01-16 07:38:43 +0900

    init

---
+++ src/main/java/com/cheatbook/TodoApplicationApi/TodoApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.cheatbook.TodoApplicationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheatbook/TodoApplicationApi/TodoApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheatbook.TodoApplicationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
