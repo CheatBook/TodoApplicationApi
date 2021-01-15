96c72e37bf6b5f3814f73ca6199ffc3a0cca3010
cheatbook <km1994321@yahoo.co.jp>
2021-01-16 08:25:06 +0900

    todo情報とtodoステータスのリポジトリー追加

---
+++ src/main/java/com/cheatbook/TodoApplicationApi/repository/TodoStatusInfoRepository.java
@@ -0,0 +1,7 @@
+package com.cheatbook.TodoApplicationApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cheatbook.TodoApplicationApi.model.TodoStatusInfo;
+
+public interface TodoStatusInfoRepository extends JpaRepository<TodoStatusInfo, Long> {
+}
