74b27c9d12917c06e00ffc6aff0b770e27c16da8
cheatbook <km1994321@yahoo.co.jp>
2021-01-16 08:25:40 +0900

    コントローラ追加

---
+++ src/main/java/com/cheatbook/TodoApplicationApi/controller/TodoApiController.java
@@ -0,0 +1,78 @@
+package com.cheatbook.TodoApplicationApi.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import com.cheatbook.TodoApplicationApi.model.TodoInfo;
+import com.cheatbook.TodoApplicationApi.repository.TodoInfoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/todo")
+public class TodoApiController {
+
+    private final TodoInfoRepository repository;
+
+    /**
+     * 一覧画面表示
+     * 
+     * @param model
+     * @return
+     */
+    @GetMapping("/list")
+    public List<TodoInfo> showList(Model model) {
+        return repository.findAll();
+    }
+
+    /**
+     * 新規登録処理
+     * 
+     * @param todoInfo
+     * @param result
+     * @return
+     */
+    @PostMapping("/registration")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registration(@RequestBody TodoInfo todoInfo) {
+        todoInfo.setRegistrationTime(LocalDateTime.now());
+        repository.save(todoInfo);
+    }
+
+    /**
+     * 編集画面表示
+     * 
+     * @param taskId
+     * @param model
+     * @return
+     */
+    @GetMapping("/edit/{taskId}")
+    public TodoInfo editData(@PathVariable Long taskId) {
+        return repository.findById(taskId).get();
+    }
+
+    /**
+     * 削除処理
+     * 
+     * @param taskId
+     * @return
+     */
+    @GetMapping("/delete/{taskId}")
+    public void deleteData(@PathVariable Long taskId) {
+        repository.deleteById(taskId);
+    }
+
+}
