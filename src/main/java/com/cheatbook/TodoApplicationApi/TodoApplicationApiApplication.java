e88f0b7cef5540dce175d0392594bc4d394ba374
kou <cheatbook_tarou@yahoo.co.jp>
2021-03-28 15:10:47 +0900

    タブをスペースに変換

--- src/main/java/com/cheatbook/TodoApplicationApi/TodoApplicationApiApplication.java
+++ src/main/java/com/cheatbook/TodoApplicationApi/TodoApplicationApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoApplicationApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplicationApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplicationApiApplication.class, args);
+    }
 
 }
